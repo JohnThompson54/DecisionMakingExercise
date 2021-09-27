1202814600512480ec675c12f664a4c23e9bde49
Your Name <you@example.com>
2021-09-27 08:42:02 -0500

    Decision Making Exercise Javascript

---
+++ script.js
@@ -0,0 +1,27 @@
+
+// 1a
+let randomNum = Math.random()
+randomNum *= 5
+console.log(randomNum);
+randomNum = Math.floor(randomNum)
+console.log(randomNum);
+randomNum++
+console.log(randomNum);
+
+// 1b
+
+if (randomNum>=4) {console.log("Greater than or equal to 4");} else if (random=2,3) {console.log("Equal to 2 or 3");} else {"Equal to 1"}
+
+// 1c
+if (randomNum!=3) {console.log("Not equal to 3");}
+
+// 1d
+if (randomNum !=(3&&5)) {console.log("NOT equal to 3 AND not equal to 5");}
+
+// 1e
+if (randomNum = 2||4) {console.log("Equal to 2 OR equal to 4");}
+
+String; msg = randNum >= 4
+    ? "Greater than or equal to 4" 
+    : "Less than 4";
+console.log(randNum);
